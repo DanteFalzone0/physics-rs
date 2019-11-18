26a001f7b9eb8bc3c16c68fc971d93a056a28633
Dante Falzone <dante.j.falzone@protonmail.com>
2019-11-18 13:08:27 -0600

    Created window

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,65 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
+
 fn main() -> Result<(), String> {
-    println!("Hello, world!");
+    // size of the window
+    let window_dim = 400;
+
+
+    let sdl_context = sdl2::init()?; // initialize the SDL stuff
+    let video_subsystem = sdl_context.video()?;
+
+    // Create an SDL window
+    let window = video_subsystem.window(
+        "Physics Simulation in Rust",
+        window_dim,
+        window_dim
+    ).position_centered().opengl().build()
+        .map_err(|e| e.to_string())?; // catch any errors
+
+    // create a new SDL rendering context
+    let mut renderer = window.into_canvas().build()
+        .map_err(|e| e.to_string())?;
+
+    // Game event loop
+    let mut event_pump = sdl_context.event_pump()?;
+    'game_loop: loop {
+        renderer.set_draw_color(Color::RGB(0, 0, 0));
+        renderer.clear();
+        renderer.present();
+
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'game_loop,
+
+                Event::KeyDown { keycode: Some(keycode), .. } => {
+                    if keycode == Keycode::X {
+                        println!("Closing the program.");
+                        break 'game_loop;
+                    } else if keycode == Keycode::A {
+                        println!("You pressed A!");
+                    }
+                },
+
+                _ => {}
+            }
+        }
+        ::std::thread::sleep(
+            Duration::new(
+                0,
+                1_000_000_000u32 / 60
+            )
+        );
+    }
+
     Ok(())
 }
