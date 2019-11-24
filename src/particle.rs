92a5334b20d10fb92b77744a636bb653cec3e783
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-11-24 15:15:33 -0600

    added enlightening comments

--- src/particle.rs
+++ src/particle.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
 
+/* As you can see this is basically exactly the
+   same as the C version: a struct of four floats. */
 #[derive (Copy, Clone)]
 pub struct Particle {
     pub x_pos:      f32,
@@ -8,7 +10,9 @@ pub struct Particle {
     pub y_momentum: f32,
 }
 
-
+/* You could call this a constructor if you wanted to
+   be fancy. It just returns a Particle with its fields
+   zero-initialized. */
 pub fn new() -> Particle {
     Particle {    
         x_pos: 0.0,
@@ -19,7 +23,13 @@ pub fn new() -> Particle {
 }
 
 
+/* I didn't need to write as many functions for the Rust
+   implementation of this program as for the C version,
+   because the Rust compiler handles many of those issues
+   of memory management. */
 impl Particle {
+    /* This function, called each frame refresh, will update
+       the state of each particle. */
     pub fn update(
         &mut self,
         grav_x: i32,
@@ -29,12 +39,15 @@ impl Particle {
         window_width: u32,
         window_height: u32,
         gravity: f32
-    ) {    
+    ) { 
+        /* Particles are moved by their momentum and slowed
+           down by drag. */
         self.x_pos += self.x_momentum;
         self.y_pos += self.y_momentum;
         self.x_momentum *= 1.0 - space_drag;
         self.y_momentum *= 1.0 - space_drag;
 
+        /* Particles bounce off of the walls. */
         if self.x_pos < 0.0 {
             self.x_pos = 0.0;
             self.x_momentum *= -1.0 * wall_bounciness;
@@ -55,6 +68,7 @@ impl Particle {
             self.y_momentum *= -1.0 * wall_bounciness;
         }
 
+        /* Particles gravitate towards the mouse. */
         let y_diff = self.y_pos - (grav_y as f32);
         let x_diff = self.x_pos - (grav_x as f32);
         let hypot = x_diff.hypot(y_diff); /* very convenient! */
@@ -62,6 +76,8 @@ impl Particle {
         self.y_momentum -= gravity * (y_diff / hypot);
     }
 
+    /* This function will get called whenever two particles get
+       very close. */
     pub fn collide_with(
         &mut self,
         other: Particle
